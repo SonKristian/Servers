6c76da6c540ead235d121252840c61a0762df389
SonKristian <103516057+SonKristian@users.noreply.github.com>
2023-05-26 12:54:52 +0200

    Server Secondo aggiornato al 26-05

---
+++ quarto_modulo/secondo_server - Copia/routesFilm.mjs
@@ -0,0 +1,18 @@
+import caseCinematografiche from "./db/case_cinematografiche.json" assert { type: 'json' }
+import film from "./db/film.json" assert { type: 'json' }
+
+const cCFilms = (req, res) => {  
+    const filtered = film.filter((f) => f.casaProduzione == req.params.id)
+    console.log(film, typeof req.params.id, filtered)
+    res.send(filtered)
+}
+
+const fFGet =(req, res) =>{
+
+    res.send(film)
+}
+
+export {
+    cCFilms,
+    fFGet
+}
